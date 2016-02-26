3e4d28a02f89ec9189a39deca92a708e5304df43
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2016-02-26 10:29:04 +0100

    added haskell test for code samples

---
+++ tests/bnfc/testparserhaskell.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Prep work that creates and prepares the bnfc parser for cpp
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "" > $DIR/haskelltest.result
+
+cd $DIR
+mkdir build
+cd build
+
+bnfc -m -haskell ../../../bnf/bnf.cf
+make
+
+# Test Files
+
+for file in $DIR/*.sppl
+do
+	$DIR/build/TestBnf $file
+	echo $?    $file >> $DIR/haskelltest.result
+done
+
+echo HASKELL RESULT
+echo
+ 
+cat $DIR/haskelltest.result
+
+# Clean-up
+
+cd $DIR
+rm -r build
