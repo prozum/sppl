b600e987ea392b445c3b9dcd68e8c38632dc9219
Jimmi Holst Christensen <rainbowhejsil@gmail.com>
2016-02-26 14:04:28 +0100

    Ast: added ast sample

---
+++ samples/ast/Ast.C
@@ -0,0 +1,11 @@
+/*** BNFC-Generated Visitor Design Pattern Reducer. ***/
+/* This implements the common visitor design pattern.
+   Note that this method uses Visitor-traversal of lists, so
+   List->accept() does NOT traverse the list. This allows different
+   algorithms to use context information differently. */
+
+#include "Ast.H"
+
+
+
+
