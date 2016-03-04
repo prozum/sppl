4d3953bed54579a9e1693208c4de66e25c3987a3
Kasper Steensig <ksj@prozum.dk>
2016-03-04 11:25:46 +0100

    compiler-c: CCodeGenerator.cpp now has all the Visitor methods defined

--- samples/compiler-c/CCodeGenerator.cpp
+++ samples/compiler-c/CCodeGenerator.cpp
@@ -1,1 +1,188 @@
 #include "CCodeGenerator.h"
+
+using namespace ast;
+
+void CodeGenerator::visit(Program *node)
+{
+    
+}
+
+void CodeGenerator::visit(Function *node)
+{
+    
+}
+
+void CodeGenerator::visit(Case *node)
+{
+    
+}
+
+void CodeGenerator::visit(Or *node)
+{
+    
+}
+
+void CodeGenerator::visit(And *node)
+{
+    
+}
+
+void CodeGenerator::visit(Equal *node)
+{
+    
+}
+
+void CodeGenerator::visit(NotEqual *node)
+{
+    
+}
+
+void CodeGenerator::visit(Lesser *node)
+{
+    
+}
+
+void CodeGenerator::visit(GreaterEq *node)
+{
+    
+}
+
+void CodeGenerator::visit(Add *node)
+{
+    
+}
+
+void CodeGenerator::visit(Sub *node)
+{
+    
+}
+
+void CodeGenerator::visit(Mul *node)
+{
+    
+}
+
+void CodeGenerator::visit(Div *node)
+{
+    
+}
+
+void CodeGenerator::visit(Mod *node)
+{
+    
+}
+
+void CodeGenerator::visit(ListAdd *node)
+{
+    
+}
+
+void CodeGenerator::visit(Par *node)
+{
+    
+}
+
+void CodeGenerator::visit(Not *node)
+{
+    
+}
+
+void CodeGenerator::visit(Int *node)
+{
+    
+}
+
+void CodeGenerator::visit(Float *node)
+{
+    
+}
+
+void CodeGenerator::visit(Bool *node)
+{
+    
+}
+
+void CodeGenerator::visit(Char *node)
+{
+    
+}
+
+void CodeGenerator::visit(String *node)
+{
+    
+}
+
+void CodeGenerator::visit(ListPattern *node)
+{
+    
+}
+
+void CodeGenerator::visit(TuplePattern *node)
+{
+    
+}
+
+void CodeGenerator::visit(ListSplit *node)
+{
+    
+}
+
+void CodeGenerator::visit(List *node)
+{
+    
+}
+
+void CodeGenerator::visit(Tuple *node)
+{
+    
+}
+
+void CodeGenerator::visit(Id *node)
+{
+    
+}
+
+void CodeGenerator::visit(Call *node)
+{
+    
+}
+
+void CodeGenerator::visit(IntType *node)
+{
+    
+}
+
+void CodeGenerator::visit(FloatType *node)
+{
+    
+}
+
+void CodeGenerator::visit(BoolType *node)
+{
+    
+}
+
+void CodeGenerator::visit(CharType *node)
+{
+    
+}
+
+void CodeGenerator::visit(StringType *node)
+{
+    
+}
+
+void CodeGenerator::visit(ListType *node)
+{
+    
+}
+
+void CodeGenerator::visit(TupleType *node)
+{
+    
+}
+
+void CodeGenerator::visit(Signature *node)
+{
+    
+}
