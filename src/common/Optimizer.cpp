5a18229497f68f5a6d85aae4a332b781515b774b
Kasper Steensig <ksj@prozum.dk>
2016-03-09 15:12:25 +0100

    semantics/optimizer: moved everything into one folder instead of splitting them up

--- src/common/Optimizer.cpp
+++ src/common/Optimizer.cpp
@@ -1,6 +1,7 @@
 #include "Optimizer.h"
 
 namespace common {
+
     Optimizer::Optimizer() {
 
     }
--- src/optimizer/ParallelOptimizer.cpp
+++ src/optimizer/ParallelOptimizer.cpp
@@ -4,6 +4,7 @@
 #include <string>
 
 using namespace std;
+using namespace common;
 
 namespace optimizer {
 
