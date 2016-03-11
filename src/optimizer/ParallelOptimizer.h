42e0c401f67c70f7aca8eb6faf7e8c051bcce6b3
Jimmi Holst Christensen <rainbowhejsil@gmail.com>
2016-03-11 08:52:38 +0100

    paralleloptimizer: fixed build errors

--- src/optimizer/ParallelOptimizer.h
+++ src/optimizer/ParallelOptimizer.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "Visitor.h"
-#include "Optimizer.h"
 #include "Node.h"
 
 #include <ostream>
@@ -11,70 +10,70 @@ using namespace common;
 
 namespace optimizer {
 
-    class ParallelOptimizer : public Optimizer {
+    class ParallelOptimizer : public Visitor {
 
-        void visit(Program *node);
+        void visit(Program &node);
 
-        void visit(Function *node);
+        void visit(Function &node);
 
-        void visit(Case *node);
+        void visit(Case &node);
 
-        void visit(Or *node);
+        void visit(Or &node);
 
-        void visit(And *node);
+        void visit(And &node);
 
-        void visit(Equal *node);
+        void visit(Equal &node);
 
-        void visit(NotEqual *node);
+        void visit(NotEqual &node);
 
-        void visit(Lesser *node);
+        void visit(Lesser &node);
 
-        void visit(Greater *node);
+        void visit(Greater &node);
 
-        void visit(LesserEq *node);
+        void visit(LesserEq &node);
 
-        void visit(GreaterEq *node);
+        void visit(GreaterEq &node);
 
-        void visit(Add *node);
+        void visit(Add &node);
 
-        void visit(Sub *node);
+        void visit(Sub &node);
 
-        void visit(Mul *node);
+        void visit(Mul &node);
 
-        void visit(Div *node);
+        void visit(Div &node);
 
-        void visit(Mod *node);
+        void visit(Mod &node);
 
-        void visit(ListAdd *node);
+        void visit(ListAdd &node);
 
-        void visit(Par *node);
+        void visit(Par &node);
 
-        void visit(Not *node);
+        void visit(Not &node);
 
-        void visit(Int *node);
+        void visit(Int &node);
 
-        void visit(Float *node);
+        void visit(Float &node);
 
-        void visit(Bool *node);
+        void visit(Bool &node);
 
-        void visit(Char *node);
+        void visit(Char &node);
 
-        void visit(String *node);
+        void visit(String &node);
 
-        void visit(ListPattern *node);
+        void visit(ListPattern &node);
 
-        void visit(TuplePattern *node);
+        void visit(TuplePattern &node);
 
-        void visit(ListSplit *node);
+        void visit(ListSplit &node);
 
-        void visit(List *node);
+        void visit(List &node);
 
-        void visit(Tuple *node);
+        void visit(Tuple &node);
 
-        void visit(Id *node);
+        void visit(Id &node);
 
-        void visit(Call *node);
+        void visit(Call &node);
 
-        void visit(Type *node);
+        void visit(Type &node);
     };
 }
