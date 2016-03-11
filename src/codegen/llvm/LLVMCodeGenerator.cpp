0b6e4fcf6dcf2726272283044b90049546a561e9
Niclas Moeslund Overby <nimoov@prozum.dk>
2016-03-11 15:57:13 +0100

    llvm: initial work on llvm

--- src/codegen/llvm/LLVMCodeGenerator.cpp
+++ src/codegen/llvm/LLVMCodeGenerator.cpp
@@ -1,7 +1,69 @@
 #include "LLVMCodeGenerator.h"
 
+using namespace llvm;
+
 namespace codegen {
-    LLVMCodeGenerator::LLVMCodeGenerator(std::ostream &out): CodeGenerator::CodeGenerator(out)
+    LLVMCodeGenerator::LLVMCodeGenerator(std::ostream &out):
+            common::CodeGenerator::CodeGenerator(out),
+            Builder(getGlobalContext()),
+            Module("SpplModule", getGlobalContext())
     {
+    }
+
+    void LLVMCodeGenerator::visit(common::Program &node) {
+        for (auto& func: node.funcs) {
+            func->accept(*this);
+        }
+    }
+
+
+    void LLVMCodeGenerator::visit(common::Function &node) {
+        for (auto& _case: node.cases) {
+            current_func = &node;
+            _case->accept(*this);
+        }
+    }
+
+    void LLVMCodeGenerator::visit(common::Case &node) {
+
+        std::vector<Type *> Doubles(node.patterns.size(), Type::getDoubleTy(getGlobalContext()));
+
+        auto func_type = FunctionType::get(Type::getDoubleTy(getGlobalContext()), Doubles, false);
+
+        auto func = Function::Create(func_type, Function::ExternalLinkage, current_func->id, &Module);
+
+        unsigned i = 0;
+        for (auto &arg : func->args())
+            arg.setName("x"+i);
+
+        BasicBlock *BB = BasicBlock::Create(getGlobalContext(), "entry", func);
+        Builder.SetInsertPoint(BB);
+
+        ContextValues.clear();
+        for (auto &arg : func->args())
+            ContextValues[arg.getName()] = &arg;
+
+        node.expr->accept(*this);
+
+        current_val = Builder.CreateRet(current_val);
+
+        current_val->dump();
+
+        verifyFunction(*func);
+    }
+
+    void LLVMCodeGenerator::visit(common::Add &node) {
+
+        node.left->accept(*this);
+        auto left = current_val;
+        node.right->accept(*this);
+        auto right = current_val;
+
+        Builder.CreateFAdd(left, right, "addtmp");
+    }
+
+    void LLVMCodeGenerator::visit(common::Float &node)
+    {
+        current_val = ConstantFP::get(getGlobalContext(), APFloat(node.value));
     }
 }
--- src/codegen/llvm/LLVMCodeGenerator.h
+++ src/codegen/llvm/LLVMCodeGenerator.h
@@ -1,14 +1,33 @@
 #pragma once
+#include <llvm/ADT/STLExtras.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Verifier.h>
+
+#include <iostream>
+
 #include "CodeGenerator.h"
 
-using namespace common;
 
 namespace codegen {
 
-    class LLVMCodeGenerator : public CodeGenerator
+    class LLVMCodeGenerator : public common::CodeGenerator
     {
     public:
         LLVMCodeGenerator(std::ostream &);
 
+        void visit(common::Program &node);
+        void visit(common::Function &node);
+        void visit(common::Case &node);
+        void visit(common::Add &node);
+        void visit(common::Float &node);
+
+    private:
+        llvm::IRBuilder<> Builder;
+        llvm::Module Module;
+        std::map<std::string, llvm::Value*> ContextValues;
+        common::Function *current_func;
+        llvm::Value *current_val;
     };
 }
