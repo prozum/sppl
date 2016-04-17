2e37554fda429f16e4ff75feaac934e94bc5a7af
Niclas Moeslund Overby <nimoov@prozum.dk>
2016-04-17 16:18:27 +0200

    sppl: move all samples to tools

---
+++ tools/interpreter/Main.cpp
@@ -0,0 +1,53 @@
+#include "Compiler.h"
+#include "SpplJit.h"
+
+#include <regex>
+
+using namespace jit;
+
+int main(int argc, char *argv[]) {
+
+    SpplJit::initLLVM();
+    SpplJit JIT;
+
+    if (argc > 1) {
+        std::ifstream f(argv[1]);
+        if (!f.good()) {
+            std::cerr << "sppl: can't open file: '" << argv[1] << "'" << std::endl;
+            return 1;
+        }
+
+        std::stringstream Buffer;
+        Buffer << f.rdbuf();
+
+        JIT.eval(Buffer.str());
+        return 0;
+    }
+
+    string Input("");
+    string InputCase("");
+    while (true)
+    {
+        cout << ">>> ";
+        getline(cin, Input);
+
+        if (Input.compare("quit") == 0 ||
+            Input.compare("exit") == 0)
+            return 0;
+
+        if (Input.compare("") == 0) {
+            cout << "Use 'quit' or 'exit' to leave" << endl;
+        } else {
+            if (!Input.compare(0, 3, "def")) {
+                do {
+                    Input += InputCase + "\n";
+                    cout << "... ";
+                    getline(cin, InputCase);
+                }
+                while (!(InputCase.compare(0, 1, "|") && InputCase.compare(0, 2, "\t|")));
+            }
+
+            JIT.eval(Input);
+        }
+    }
+}
