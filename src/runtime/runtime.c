6e6c95aef0bd011c56b85b30f57f4fb7b2d16be7
Kasper Steensig <ksj@prozum.dk>
2016-04-17 22:47:03 +0200

    more work on runtime

--- src/runtime/runtime.c
+++ src/runtime/runtime.c
@@ -1,127 +1,137 @@
 #include "runtime.h"
+#include <stdio.h>
 
 void add_task(task_t *task) {
 
-    task->_state = NEW;
-    task->done = 0;
+    task->state = NEW;
 
     queue_head *head = malloc(sizeof(queue_head));
     head->item = task;
 
-    queue_add(head, queue);
+    queue_add(head, runtime.queue);
 }
 
 int check_subtasks_done(task_t *task) {
 
-    uint32_t acc = 0;
+    printf("checking subtasks\n");
 
     for (uint32_t i = 0; i < task->sub_task_len; ++i) {
-        acc += task->done;
+
+        if (task->state != DONE) {
+            return 0;
+        }
     }
 
-    return acc == task->sub_task_len;
+    return 1;
 }
 
-void switch_task(task_t *task) {
-    task->_state = WAITING;
+void yield(task_t *task) {
+    task->state = WAITING;
 
     queue_head *head = malloc(sizeof(queue_head));
     head->item = task;
-    queue_add(head, queue);
+    queue_add(head, runtime.queue);
 
-    push_context();
+    task->context = malloc(sizeof(ucontext_t));
 
-    swtch_context(&(task->_context), scheduler_pool[task->scheduler_id].context);
+    printf("entered yield - %lu\n", task->scheduler_id);
 
-    pop_context();
+    swapcontext(task->context, runtime.scheduler_pool[task->scheduler_id].context);
 }
 
 //runtime entry point
-void rmain(uint64_t count, task_t *initial_task) {
-
-    // initialize pool of schedulers/workers and threads
-    scheduler_pool = malloc(sizeof(scheduler_t) * count);
-    thread_pool = malloc(sizeof(pthread_t) * count);
-
-    // initialize global variables
-    queue = create_queue();
-    g_count = count;
-    pthread_mutex_init(scheduler_states_lock, NULL);
-    scheduler_states = malloc(sizeof(scheduler_state_e) * count);
+void rmain(uint64_t os_thread_count, task_t *initial_task) {
+    runtime.os_thread_count = os_thread_count;
+    runtime.queue = create_queue();
+    runtime.scheduler_pool = malloc(sizeof(scheduler_t) * os_thread_count);
+    runtime.thread_pool = malloc(sizeof(pthread_t) * os_thread_count);
+    runtime.scheduler_status = malloc(sizeof(scheduler_state_t) * os_thread_count);
+    pthread_mutex_init(&runtime.scheduler_status_lock, NULL);
 
     add_task(initial_task);
 
-    for (uint64_t i = 0; i < count; ++i) {
-        scheduler_pool[i].id = i;
-        scheduler_pool[i].io_worker = malloc(sizeof(pthread_t));
-        scheduler_pool[i].queue = queue;
-        thread_pool[i] = pthread_create(&(thread_pool[i]), NULL, (void *)run_scheduler, (void *)(&scheduler_pool[i]));
+    for (uint64_t i = 0; i < os_thread_count; ++i) {
+        runtime.scheduler_pool[i].id = i;
+        runtime.scheduler_pool[i].io_worker = malloc(sizeof(pthread_t));
+        pthread_create(&runtime.thread_pool[i], NULL, (void *)start_scheduler, &runtime.scheduler_pool[i]);
     }
 
-    for (uint64_t j = 0; j < count; ++j) {
-        pthread_join(thread_pool[j], NULL);
+    for (uint64_t j = 0; j < os_thread_count; ++j) {
+        pthread_join(runtime.thread_pool[j], NULL);
     }
 
-    free(scheduler_pool);
-    free(thread_pool);
-    free(scheduler_states);
+    free(runtime.scheduler_pool);
+    free(runtime.thread_pool);
+    free(runtime.scheduler_status);
 
-    pthread_mutex_destroy(scheduler_states_lock);
+    pthread_mutex_destroy(&runtime.scheduler_status_lock);
 }
 
-void run_scheduler(scheduler_t *sched) {
+void start_scheduler(void *sched_ptr) {
+
+    scheduler_t *scheduler = (scheduler_t *)sched_ptr;
+
+    scheduler->context = malloc(sizeof(ucontext_t));
+
     queue_head *curr;
     task_t *curr_task;
 
-    save_scheduler_context(sched->context);
-
     do {
-        curr = queue_get(sched->queue);
+        getcontext(scheduler->context);
+
+        curr = queue_get(runtime.queue);
+
+        printf("inside run_scheduler do-while\n");
 
         if (curr != NULL) {
+
             curr_task = (task_t *)(curr->item);
-            set_active_workers(sched->id, WORKING);
-            if ((curr_task->_state == WAITING) && check_subtasks_done(curr_task)) {
-                sched->curr_task = curr_task;
-                curr_task->scheduler_id = sched->id;
-                swtch_context(&sched->context, curr_task->_context);
-            } else if (curr_task->_state == NEW) {
-                sched->curr_task = curr_task;
-                sched->curr_task->_state = ACTIVE;
+            curr_task->scheduler_id = scheduler->id;
+            set_active_worker(scheduler->id, WORKING);
+            if ((curr_task->state == WAITING) && check_subtasks_done(curr_task)) {
+                scheduler->curr_task = curr_task;
+                swapcontext(scheduler->context, curr_task->context);
+            } else if (curr_task->state == NEW) {
+                printf("inside run_scheduler NEW\n");
+                scheduler->curr_task = curr_task;
+                scheduler->curr_task->state = ACTIVE;
                 curr_task->f(curr_task->args);
-                sched->curr_task->done = 1;
-                sched->curr_task->_state = DONE;
+                scheduler->curr_task->state = DONE;
             } else {
-                queue_add(curr, sched->queue);
+                queue_add(curr, runtime.queue);
             }
         } else if (get_active_workers() == 0) {
             break;
         } else {
-            set_active_workers(sched->id, SLACKING);
+            set_active_worker(scheduler->id, SLACKING);
         }
     } while (1);
+
+    printf("inside run_scheduler about to free context\n");
+
+    free(scheduler->context);
 
     pthread_exit(NULL);
 }
 
 uint64_t get_active_workers() {
-    pthread_mutex_lock(scheduler_states_lock);
+    pthread_mutex_lock(&runtime.scheduler_status_lock);
 
     uint64_t acc = 0;
 
-    for (uint64_t i = 0; i < g_count; ++i) {
-        acc += scheduler_states[i];
+    for (uint64_t i = 0; i < runtime.os_thread_count; ++i) {
+        acc += runtime.scheduler_status[i];
     }
 
-    pthread_mutex_unlock(scheduler_states_lock);
+    pthread_mutex_unlock(&runtime.scheduler_status_lock);
 
     return acc;
 }
 
-void set_active_workers(uint64_t id, scheduler_state_e state) {
-    pthread_mutex_lock(scheduler_states_lock);
+void set_active_worker(uint64_t id, scheduler_state_t state) {
+    pthread_mutex_lock(&runtime.scheduler_status_lock);
 
-    scheduler_states[id] = state;
+    runtime.scheduler_status[id] = state;
 
-    pthread_mutex_unlock(scheduler_states_lock);
+    pthread_mutex_unlock(&runtime.scheduler_status_lock);
 }
--- src/runtime/runtime.h
+++ src/runtime/runtime.h
@@ -2,63 +2,65 @@
 #define SPPL_RUNTIME_H
 
 #include <pthread.h>
-#include <semaphore.h>
+#include <ucontext.h>
 
 #include "queue.h"
-#include "regs.h"
+#include "task.h"
 
-typedef struct task_s task_t;
 typedef void (*func)(void *);
 
 typedef enum task_state_e {
-    NEW,
-    WAITING,
-    BLOCKED,
-    ACTIVE,
-    DONE,
+    NEW     = 0,
+    WAITING = 1,
+    ACTIVE  = 2,
+    BLOCKED = 3,
+    DONE    = 4,
 } task_state_t;
 
-typedef enum scheduler_state {
-    SLACKING,
-    WORKING
-} scheduler_state_e;
-
-struct task_s {
+typedef struct task_s {
     uint64_t scheduler_id;
-    context_t *_context;
-    task_state_t _state;
-    char done;
+    ucontext_t *context;
+    task_state_t state;
     void *args;
     func f;
     uint32_t sub_task_len;
-    task_t **sub_tasks;
-};
+    struct task_s **sub_tasks;
+} task_t;
+
+void add_task(task_t *task);
+int check_subtasks_done(task_t *task);
+void yield(task_t *task);
+
+typedef enum scheduler_state_e {
+    SLACKING,
+    WORKING
+} scheduler_state_t;
 
 typedef struct scheduler_s {
     uint64_t id;
-    context_t *context;
+    ucontext_t *context;
     task_t *curr_task;
-    queue_root *queue;
     pthread_t *io_worker;
 } scheduler_t;
 
-//global variables
-uint64_t g_count;
-queue_root *queue;
-pthread_t *thread_pool;
-scheduler_t *scheduler_pool;
-pthread_mutex_t *scheduler_states_lock;
-scheduler_state_e *scheduler_states;
+typedef struct runtime_s {
+    uint64_t os_thread_count;
+    pthread_t *thread_pool;
+    scheduler_t *scheduler_pool;
 
-//task functions
-void add_task(task_t *task);
-int check_subtasks_done(task_t *task);
-void switch_task(task_t *task);
+    queue_root *queue;
 
-//scheduler and worker functions
+    pthread_mutex_t scheduler_status_lock;
+    scheduler_state_t *scheduler_status;
+} runtime_t;
+
+
+// global runtime
+runtime_t runtime;
+
 void rmain(uint64_t sched_count, task_t *initial_task);
-void run_scheduler(scheduler_t *sched);
+void start_scheduler(void *sched_ptr);
 uint64_t get_active_workers();
-void set_active_workers(uint64_t id, scheduler_state_e state);
+void set_active_worker(uint64_t id, scheduler_state_t state);
 
 #endif //SPPL_RUNTIME_H
