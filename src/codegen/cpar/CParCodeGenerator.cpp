d259b33641200fb94f4f7af86a61c9bfb7216869
hejsil <rainbowhejsil@gmail.com>
2016-04-28 11:47:49 +0200

    cparcodegen: some refactoring and print newline

--- src/codegen/cpar/CParCodeGenerator.cpp
+++ src/codegen/cpar/CParCodeGenerator.cpp
@@ -12,7 +12,7 @@ void CParCodeGenerator::visit(Program &Node) {
     Prog = &Node;
     Function *Main = nullptr;
 
-    *Header << "#include \"task.h\" \n";
+    *Header << "#include \"task.h\" " << endl;
 
     // Generate the standard functionality for every program
     generateStd();
@@ -35,25 +35,26 @@ void CParCodeGenerator::visit(Program &Node) {
     // Generate real main function, that calls the users main
     string StrListName = getList(StringList);
 
-    *Output << "int main(int argc, char** argv) { \n"
-            "    " << StrListName << " *args = " << GGenerated << GCreate << StrListName << "(0); \n"
-            "    int i; \n"
-            " \n"
-            "    for(i = argc - 1; i >= 0; i--) { \n"
-            "        args = " << GGenerated << GAdd << StrListName << "(args, " << GGenerated
-                                            << GCreate << GString << "(argv[i])); \n"
-            "    } \n"
-            " \n"
-            "    " << MainSig << GArg << "* main_args = malloc(sizeof(" << MainSig << GArg << ")); \n"
-            "    main_args->" << GGenerated << GArg << "0 = args; \n"
-            "    task_t *main_task = taskcreate((void *)&" << GUser << GMain << ", (void *)main_args); \n"
-            "    rmain(4, main_task); \n"
-            "\n"
-            "    " << Prints[Main->Signature.Subtypes.back()] << "(main_args->" << GGenerated << GRes << "); \n"
-            "    taskdealloc(main_task); \n"
-            "    return 0; \n"
-            "} \n"
-            " \n";
+    *Output << "int main(int argc, char** argv) { " << endl
+            << "    " << StrListName << " *args = " << GGenerated << GCreate << StrListName << "(0); " << endl
+            << "    int i; " << endl
+            << " " << endl
+            << "    for(i = argc - 1; i >= 0; i--) { " << endl
+            << "        args = " << GGenerated << GAdd << StrListName << "(args, " << GGenerated
+                                            << GCreate << GString << "(argv[i])); " << endl
+            << "    } " << endl
+            << " " << endl
+            << "    " << MainSig << GArg << "* main_args = malloc(sizeof(" << MainSig << GArg << ")); " << endl
+            << "    main_args->" << GGenerated << GArg << "0 = args; " << endl
+            << "    task_t *main_task = taskcreate((void *)&" << GUser << GMain << ", (void *)main_args); " << endl
+            << "    rmain(4, main_task); " << endl
+            << "" << endl
+            << "    " << Prints[Main->Signature.Subtypes.back()] << "(main_args->" << GGenerated << GRes << "); " << endl
+            << "    printf(\"\\n\");" << endl
+            << "    taskdealloc(main_task); " << endl
+            << "    return 0; " << endl
+            << "} " << endl
+            << " " << endl;
 
 
     for (auto &Func : Node.Decls) {
@@ -76,10 +77,10 @@ void CParCodeGenerator::visit(Function &Node) {
     Func << "void " << GUser << Node.Id << "(task_t *t)";
 
     // Generate function decleration in header
-    *Header << Func.str() << "; \n \n";
+    *Header << Func.str() << "; \n " << endl;
 
     // Generate function in *output
-    *Output << Func.str() << " { \n";
+    *Output << Func.str() << " { " << endl;
 
     for (size_t i = 0; i < Node.Signature.subtypeCount() - 1; ++i) {
         ArgType = getType(Node.Signature.Subtypes[i]);
@@ -87,13 +88,13 @@ void CParCodeGenerator::visit(Function &Node) {
         ArgNames.push_back(ArgName.str());
         *Output << "    " << ArgType << " " << ArgName.str() << " = "
                    "(" << ArgType << ")" << "(" << "(" << CurrentArg << "*)" << "t->startarg)->"
-                << ArgName.str() << ";\n";
+                << ArgName.str() << ";" << endl;
 
         ArgName.str("");
     }
 
-    *Output << "Start: \n"
-               "\n";
+    *Output << "Start: " << endl
+            << endl;
 
     // Generate cases
     for (auto &Case : Node.Cases) {
@@ -106,10 +107,10 @@ void CParCodeGenerator::visit(Function &Node) {
     }
 
     // Generate error, for when program doesn't realize a case
-    *Output << "    printf(\"No cases realized!\\n\"); \n"
-            "    exit(1); \n"
-            "} \n"
-            " \n";
+    *Output << "    printf(\"No cases realized!\\n\"); " << endl
+            << "    exit(1); " << endl
+            << "} " << endl
+            << endl;
 
     // Clear ArgNames for current function
     ArgNames.clear();
@@ -152,7 +153,7 @@ void CParCodeGenerator::visit(Case &Node) {
         *Output << Pattern.str() << endl;
     }
 
-    *Output << "    { \n";
+    *Output << "    { " << endl;
 
     // Generate all nessesary assigments
     for (auto &Assign : Assignments) {
@@ -170,12 +171,12 @@ void CParCodeGenerator::visit(Case &Node) {
 
         for (size_t i = CallStack.size(); i > 0; i--) {
             *Output << CallStack.back();
-            *Output << "        taskyield(t);\n";
+            *Output << "        taskyield(t);" << endl;
             CallStack.pop_back();
         }
 
-        *Output << "        if (" << ExprStack.top().str() << ") \n"
-                   "        { \n";
+        *Output << "        if (" << ExprStack.top().str() << ") " << endl
+                << "        { " << endl;
         ExprStack.pop();
     }
 
@@ -190,17 +191,17 @@ void CParCodeGenerator::visit(Case &Node) {
 
             for (size_t i = CallStack.size(); i > 0; i--) {
                 *Output << CallStack.back();
-                *Output << "        taskyield(t);\n";
+                *Output << "        taskyield(t);" << endl;
                 CallStack.pop_back();
             }
 
             *Output << "        " << GGenerated << GArg << i << " = "
-                                  << ExprStack.top().str() << "; \n";
+                                  << ExprStack.top().str() << "; " << endl;
             ExprStack.pop();
         }
 
-        *Output << "\n"
-                "        goto Start; \n";
+        *Output << endl
+                << "        goto Start; " << endl;
     } else {
         string ResType = getType(CurFunc->Signature.Subtypes.back());
         ExprStack.push(stringstream());
@@ -209,21 +210,22 @@ void CParCodeGenerator::visit(Case &Node) {
 
         for (size_t i = CallStack.size(); i > 0; i--) {
             *Output << CallStack.back();
-            *Output << "        taskyield(t);\n";
+            *Output << "        taskyield(t);" << endl;
             CallStack.pop_back();
         }
 
-        *Output << "        ((" << CurrentArg << "*)t->startarg)->" << GGenerated << GRes << " = " << ExprStack.top().str() << "; \n";
-        *Output << "        taskexit(t); \n";
+        *Output << "        ((" << CurrentArg << "*)t->startarg)->" << GGenerated << GRes << " = "
+                                << ExprStack.top().str() << "; " << endl;
+        *Output << "        taskexit(t); " << endl;
         ExprStack.pop();
     }
 
     if (Node.When) {
-        *Output << "        } \n";
+        *Output << "        } " << endl;
     }
 
-    *Output << "    } \n"
-            " \n";
+    *Output << "    } " << endl
+            << endl;
 }
 
 void CParCodeGenerator::visit(common::CallExpr &Node) {
@@ -240,12 +242,14 @@ void CParCodeGenerator::visit(common::CallExpr &Node) {
     CallDepth++;
 
 
-    GeneratedCall << "        " << Signature << GArg << "* " << Name << GArg << " = malloc(sizeof(" << Signature << GArg << "));\n";
+    GeneratedCall << "        " << Signature << GArg << "* " << Name << GArg << " = malloc(sizeof("
+                                << Signature << GArg << "));" << endl;
     for (size_t i = 0; i < Node.Args.size(); ++i) {
         ExprStack.push(stringstream());
         Node.Args[i]->accept(*this);
 
-        GeneratedCall << "        " << Name << GArg << "->" << GGenerated << GArg << i << " = " << ExprStack.top().str() << "; \n";
+        GeneratedCall << "        " << Name << GArg << "->" << GGenerated << GArg << i << " = "
+                                    << ExprStack.top().str() << "; " << endl;
 
         ExprStack.pop();
     }
@@ -254,8 +258,8 @@ void CParCodeGenerator::visit(common::CallExpr &Node) {
     Node.Callee->accept(*this);
 
     GeneratedCall << "        task_t *" << Name << " = taskcreate((void *)&" << ExprStack.top().str()
-                                              << ", (void *)" << Name << GArg << ");\n";
-    GeneratedCall << "        subtaskadd(t, " << Name << "); \n";
+                                              << ", (void *)" << Name << GArg << ");" << endl;
+    GeneratedCall << "        subtaskadd(t, " << Name << "); " << endl;
 
     ExprStack.pop();
     ExprStack.top() << "((" << Signature << GArg << "*)" << Name << "->startarg" << ")->" << GGenerated << GRes;
@@ -267,14 +271,14 @@ std::string CParCodeGenerator::generateEnvironment(common::Type &Ty) {
     string Name = GGenerated + GSignature + to_string(++SigCount);
     stringstream Res;
 
-    Res << "typedef struct " << Name << GArg << " {\n";
+    Res << "typedef struct " << Name << GArg << " {" << endl;
 
     for (size_t i = 0; i < Ty.subtypeCount() - 1; ++i) {
-        Res << "    " << getType(Ty.Subtypes[i]) << " " << GGenerated << GArg << i << ";\n";
+        Res << "    " << getType(Ty.Subtypes[i]) << " " << GGenerated << GArg << i << ";" << endl;
     }
 
-    Res << "    " << getType(Ty.Subtypes.back()) << " " << GGenerated << GRes << "; \n";
-    Res << "} " << Name << GArg << "; \n";
+    Res << "    " << getType(Ty.Subtypes.back()) << " " << GGenerated << GRes << "; " << endl;
+    Res << "} " << Name << GArg << "; " << endl;
 
     /*
     Res << "typedef " << getType(Ty.Subtypes.back()) << "(*" << Name << ")(";
@@ -286,12 +290,12 @@ std::string CParCodeGenerator::generateEnvironment(common::Type &Ty) {
             Res << ", ";
     }
 
-    Res << "); \n";
+    Res << "); " << endl;
      */
 
     *Header << Res.str();
 
-    *Header << "typedef void (* " << Name << ")(task_t*);\n";
+    *Header << "typedef void (* " << Name << ")(task_t*);" << endl;
 
     Closures[Ty] = Name;
     return Name;
